bddd9b3e23c4756884571581f0acc1e816dfda5a
keta124 <keta124@gmail.com>
2018-04-10 16:37:38 +0700

    Update centos7_setup.sh

--- centos7_setup.sh
+++ centos7_setup.sh
@@ -78,3 +78,20 @@ echo "EDIT SELINUX"
 sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
+#cmdlog
+echo "export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug \"[\$(echo \$SSH_CLIENT | cut -d\" \" -f1)] # \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" )\"'" >>/etc/bashrc
+echo "local6.debug                /var/log/cmdlog.log" >> /etc/rsyslog.conf
+>/var/log/cmdlog.log
+service rsyslog restart
+echo "/var/log/cmdlog.log {
+  create 0644 root root
+  compress
+  weekly
+  rotate 12
+  sharedscripts
+  postrotate
+  /bin/kill -HUP \`cat /var/run/syslogd.pid 2> /dev/null\` 2> /dev/null || true
+  endscript
+}" >>/etc/logrotate.d/cmdlog
+
+
