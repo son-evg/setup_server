38210cc64a5929348916121961a220081ae78404
keta124 <keta124@gmail.com>
2018-09-30 15:46:58 +0700

    Update squid.sh

--- squid/squid.sh
+++ squid/squid.sh
@@ -2,4 +2,6 @@ yum install squid httpd-tools -y
 htpasswd -b -c /etc/squid/passwd admin Admin@1123##
 chown -R squid:squid /etc/squid/passwd
 wget https://raw.githubusercontent.com/keta124/setup_server/master/squid/squid.conf - O /etc/squid/squid.conf
+systemctl enable squid
+systemctl start squid
 echo "Edit iptables"
