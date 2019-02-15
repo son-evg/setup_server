3b490a7c0e9452807c140b5059a1001ff75f186c
keta124 <keta124@gmail.com>
2019-02-15 11:20:55 +0700

    Update squid.sh

--- squid/squid.sh
+++ squid/squid.sh
@@ -4,4 +4,4 @@ chown -R squid:squid /etc/squid/passwd
 wget https://raw.githubusercontent.com/keta124/setup_server/master/squid/squid.conf -O /etc/squid/squid.conf
 systemctl enable squid
 systemctl start squid
-echo "Edit iptables"
+echo "Edit iptables open port 3800"
