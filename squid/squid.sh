0437c83c94eadbea174e280aa7cbfe8b2a109898
son-evg <55246868+son-evg@users.noreply.github.com>
2019-09-14 23:01:58 +0700

    Update squid.sh

--- squid/squid.sh
+++ squid/squid.sh
@@ -1,5 +1,5 @@
 yum install squid httpd-tools -y
-htpasswd -b -c /etc/squid/passwd admin Admin@1123##
+htpasswd -b -c /etc/squid/passwd admin Admin@1123
 chown -R squid:squid /etc/squid/passwd
 wget https://raw.githubusercontent.com/keta124/setup_server/master/squid/squid.conf -O /etc/squid/squid.conf
 systemctl enable squid
